ea6dbfaa0deb6b5f6a6497a9c8c0093809147002
Chameleon Cloud User <hokelvin9@gmail.com>
2018-05-08 18:13:17 +0000

    fix5

--- storage/row.cpp
+++ storage/row.cpp
@@ -26,6 +26,7 @@ row_t::init(table_t * host_table, uint64_t part_id, uint64_t row_id) {
   mark = (bool *) _mm_malloc(sizeof(bool) * g_thread_cnt, 64);
   home_mark = (bool *) _mm_malloc(sizeof(bool) * g_thread_cnt, 64);
   memset(mark, 0, g_thread_cnt * sizeof(bool));
+  memset(home_mark, 0, g_thread_cnt * sizeof(bool));
 	return RCOK;
 }
 void
@@ -33,6 +34,7 @@ row_t::init(int size)
 {
 	data = (char *) _mm_malloc(size, 64);
   mark = (bool *) _mm_malloc(sizeof(bool) * g_thread_cnt, 64);
+  home_mark = (bool *) _mm_malloc(sizeof(bool) * g_thread_cnt, 64);
 }
 
 RC
