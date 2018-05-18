1aeafc742d85b2ee72d5e0a8bfb2324b59691b60
Chameleon Cloud User <hokelvin9@gmail.com>
2018-05-18 01:19:59 +0000

    new

---
+++ comp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp silo_config.h config.h
+cp ./system/silo_main.temp ./system/main.cpp
+cp silo_make Makefile
+make clean
+make -j
+cp hstore_config.h config.h
+cp ./system/hstore_main.temp ./system/main.cpp
+cp hstore_make Makefile
+make clean
+make -j
+cp no_wait_config.h config.h
+cp ./system/no_wait_main.temp ./system/main.cpp
+cp no_wait_make Makefile
+make clean
+make -j
